9c6b05bfa7ac0106e2bc2759967a0e8827e781ce
ShuyunLiuRun <lsy2601162193@gmail.com>
2020-07-23 15:50:16 +1200

    structure rebuild

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import Table from './Table'
+import Main from './components/Main.js'
 
 const data = require('./data.json')
 
-function App({items}) {
+// function App({items}) {
   
-  const table = data.map(d => <Table data={d} key={d.ID}/>);
-  return (
-    <div className="App">
-        {table}
-        <p>{items}</p>
-    </div>
-  );
+//   const table = data.map(d => <Table data={d} key={d.ID}/>);
+//   return (
+//     <div className="App">
+//         {table}
+//         <p>{items}</p>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = data
+  }
+
+  render(){
+    const {Data} = this.state
+    return(
+      <Main Data={Data}/> 
+    )
+  }
 }
 // const {render} = ReactDOM;
 
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Data from '../data.json'
+
+
+const Main = ({Data}) =>
+    <div className="One-Item">
+        
+    </div>
+
