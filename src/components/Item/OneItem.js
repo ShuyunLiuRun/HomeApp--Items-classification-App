40efc967269b8cbf8962154abc3ec70688013ed1
ShuyunLiuRun <lsy2601162193@gmail.com>
2020-09-08 18:39:08 +1200

    meterialui card

--- src/components/Item/OneItem.js
+++ src/components/Item/OneItem.js
@@ -1,23 +1,59 @@
 // each item is wrapped in a litte square container,
 // TODO: set the css in this file(containers' size, background color...)
 // TODO: Show the item's name in the suqare
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const OneItem = ({data}) =>{
-    console.log(data)
-    const name = data.name
-    return(
-    <div className="item" style={itemStyle}>{name}</div>
-    )
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    margin: 20,
+  },
+
+});
+
+var OneItem = ({data}) =>  {
+  const classes = useStyles();
+  const {name} = data;
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={require ("./defaultImage.png")}
+          title={name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {/* Descriptions  */}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Add Item
+        </Button>
+        <Button size="small" color="primary">
+          Details
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
 
-const itemStyle = {
-    width:"80px",
-    backgroundColor:"#DCDCDC",
-    padding:"30px 0",
-    margin:"10px",
-    borderRadius:"10px",
-    textAlign:"center"
-}
+
+
 
 export default OneItem
