4276ecad77f09ff1fff758b3c368df985e1f9794
Kami <kidfu@live.com>
2020-09-25 23:24:04 +1200

    Modify migration and data scripts to suit chinese char

--- scripts/migration/master.mvp.sql
+++ scripts/migration/master.mvp.sql
@@ -1,20 +1,21 @@
-create schema if not exists homeapp;
+CREATE SCHEMA IF NOT EXISTS homeapp;
+ALTER SCHEMA homeapp CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-use homeapp;
+USE homeapp;
 
-drop table if exists master;
+DROP TABLE IF EXISTS master;
 
-create table master
+CREATE TABLE master
 (
-    Id             SMALLINT UNSIGNED auto_increment,
-    Name           VARCHAR(32)   not null,
-    ContainedBy    TINYINT(1)    not null,
-    IsContainer    TINYINT(1)    not null,
-    Level          TINYINT       not null,
-    AdditionalJson VARCHAR(1024) null,
-    constraint master
-        primary key (id)
-)
-    comment 'Master table for HomeApp MVP';
+    Id             SMALLINT UNSIGNED AUTO_INCREMENT,
+    Name           VARCHAR(32) CHARACTER SET utf8mb4   DEFAULT ''   NOT NULL,
+    ContainedBy    TINYINT(1) UNSIGNED                              NOT NULL,
+    IsContainer    TINYINT(1)                                       NOT NULL,
+    Level          TINYINT UNSIGNED                                 NOT NULL,
+    AdditionalJson VARCHAR(1024) CHARACTER SET utf8mb4 DEFAULT NULL NULL,
+    CONSTRAINT master
+        PRIMARY KEY (id)
+) ENGINE = InnoDB
+    COMMENT 'Master table for HomeApp MVP';
 
 
