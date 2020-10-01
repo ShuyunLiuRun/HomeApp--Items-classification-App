cb6a64919da981bc95bb24d1b36b048e76c5ff8d
ShuyunLiuRun <lsy2601162193@gmail.com>
2020-10-01 19:06:45 +1300

    listen 8000

--- src/Table.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Table(props){
-    const {name, positionID, isContainer, fields} = props.data;
-    return (
-    <p>{name} - {positionID} - {isContainer} - {fields}</p>
-    )
-}
-export default Table;
